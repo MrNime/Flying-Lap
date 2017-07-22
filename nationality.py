f52cc1afd55c3a0dd05d00e4b1232813ef51a80d
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-07-22 14:14:55 +0200

    changed nationality to flag

--- nationality.py
+++ nationality.py
@@ -4,20 +4,21 @@ import pandas as pd
 df = pd.read_csv('countries.csv')
 # print (df.get_value(1,'Nationality'))
 
-#set index to country name
-df.set_index(['Country'], inplace = True)
+#set index to country num_code
+df.set_index(['num_code'], inplace = True)
 
-#make code column lowercase
-df['Code'] = df['Code'].str.lower()
+# make code column lowercase
+df['alpha_2_code'] = df['alpha_2_code'].str.lower()
 
-#example usage
-# fincode = df.loc[df['Nationality'] == 'Spanish','Code']
-# print(fincode[0])
+# #example usage
+# code = df.loc[df['nationality'] == 'British','alpha_2_code']
+# print(code.values[0])
+
 
 def nationality_to_code(nationality):
     """
     function to look up country code corresponding to
     the nationality.
     """
-    codeseries = df.loc[df['Nationality'] == nationality, 'Code']
-    return codeseries[0]
+    code = df.loc[df['nationality'] == nationality,'alpha_2_code']
+    return code.values[0]
